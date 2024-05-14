240f335b4c51fea87f355fdda5ffcffcae7763c7
Nirmal Ram <nirmal@mozark.ai>
2024-05-15 03:09:09 +0530

    feature/api

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { validator, hashPassword , passwordCheck , generateToken} = require("./helper/helper");
+const {
+  validator,
+  hashPassword,
+  passwordCheck,
+  generateToken,
+  decoder,
+} = require("./helper/helper");
 const { authorizer } = require("./middleware/authorizer");
 const CustomDB = require("./database/customDb");
 const db = new CustomDB();
@@ -26,22 +32,22 @@ app.post("/register", async (req, res) => {
     if (exist) throw new Error("User already exist");
     db.insertOne(payload);
     db.printAll();
-    result = "User registered successfully";
+    result = { result : "User registered successfully"};
   } catch (error) {
     status = 400;
     result = error.message;
   }
-  res.status(status).send({result});
+  res.status(status).send(result);
 });
 
-app.post("/login", async(req, res) => {
+app.post("/login", async (req, res) => {
   let status = 200;
   let result;
   try {
     validator(req.body, "login");
-    const userInfo = db.findOne(req.body.email)
-    if(!userInfo) throw new Error('Incorrect user email')
-    await passwordCheck(req.body.password , userInfo.password)
+    const userInfo = db.findOne(req.body.email);
+    if (!userInfo) throw new Error("Incorrect user email");
+    await passwordCheck(req.body.password, userInfo.password);
     result = {
       token: await generateToken(userInfo),
     };
@@ -56,11 +62,16 @@ app.get("/preferences", authorizer, (req, res) => {
   let status = 200;
   let result;
   try {
+    const token = req.headers.authorization;
+    const userInfo = decoder(token.split(" ")[1]);
+    result = {
+      preferences: userInfo.preferences
+    }
   } catch (error) {
     status = 401;
     result = error.message;
   }
-  res.status(status).send({result});
+  res.status(status).send(result);
 });
 
 app.put("/preferences", authorizer, (req, res) => {
@@ -72,7 +83,7 @@ app.put("/preferences", authorizer, (req, res) => {
     status = 401;
     result = error.message;
   }
-  res.status(status).send({result});
+  res.status(status).send({ result });
 });
 
 app.get("/news", authorizer, (req, res) => {
@@ -83,7 +94,7 @@ app.get("/news", authorizer, (req, res) => {
     status = 401;
     result = error.message;
   }
-  res.status(status).send({result});
+  res.status(status).send({ result });
 });
 
 app.listen(port, (err) => {
